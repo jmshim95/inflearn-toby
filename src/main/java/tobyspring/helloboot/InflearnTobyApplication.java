2aac1df8406a15deca05b743bf0f289632ef4be1
jmshim95 <jmshim95@gmail.com>
2023-02-12 16:57:43 +0900

    상수 리팩토링, 리퀘스트 파마리터 받기

--- src/main/java/tobyspring/helloboot/InflearnTobyApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobyApplication.java
@@ -2,6 +2,9 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,9 +20,11 @@ public class InflearnTobyApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE);
+                    resp.getWriter().println("Hello " + name);
                 }
             }).addMapping("/hello");
         });
